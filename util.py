10987e519055a91dd834b29f150ea9fe13fad6b8
Ayoosh <kathuria.ayoosh@gmail.com>
2018-03-05 17:18:03 +0530

    Functions to convert cuda array to GPU

---
+++ util.py
@@ -0,0 +1,21 @@
+from __future__ import division
+
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F 
+from torch.autograd import Variable
+import numpy as np
+import cv2 
+import matplotlib.pyplot as plt
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters())
+
+def count_learnable_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def convert2cpu(matrix):
+    if matrix.is_cuda:
+        return torch.FloatTensor(matrix.size()).copy_(matrix)
+    else:
+        return matrix
