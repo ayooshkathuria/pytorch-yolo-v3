a0f1daf1b49c48ae2463f220aca7af1aba0de153
Ayoosh <kathuria.ayoosh@gmail.com>
2018-03-12 10:29:10 +0530

    Function for filtering out boxes w.r.t. a confidence threshold

--- bbox.py
+++ bbox.py
@@ -7,13 +7,18 @@ from torch.autograd import Variable
 import numpy as np
 
 
-def confidence_filter(result, confidence):
+def confidence_filter(result, confidence, num_classes, inp_dim):
     batch_size = result.size(0)
-    result = result.view(-1,13*13).contiguous()
-    result = result.view(-1, 5*13*13).transpose(0,1).contiguous()
+    network_stride = 32
+    grid_size = inp_dim // network_stride
+    bbox_attrs = 5 + num_classes
     
-    conf_mask = (torch.sigmoid(result[:, 4]) > confidence).view(-1,1).float()
-    result_mask = result*conf_mask
-    mask_index = torch.nonzero(result_mask[:,4].data)
-    result_mask = result_mask[mask_index, :].squeeze()
-    print (result_mask.data.shape)
+    
+    
+    result = result.view(batch_size, -1, grid_size*grid_size)
+    result = result.view(batch_size, bbox_attrs, -1)
+    
+    result = result.transpose(1,2).contiguous()
+    conf_mask = (torch.sigmoid(result[:,:,4]) > confidence).float().unsqueeze(2)
+    result = result*conf_mask
+    return result
