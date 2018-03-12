39c872c7374446e67ab72339edda20b52a3bb0e1
Ayoosh <kathuria.ayoosh@gmail.com>
2018-03-12 11:18:27 +0530

    Function to create batches from a list of image paths

--- preprocess.py
+++ preprocess.py
@@ -24,3 +24,22 @@ def prep_image(img, network_dim):
     img_ = torch.from_numpy(img_).float()
     img_ = Variable(img_)
     return img_
+
+def batchify(imlist, batch_size, network_dim):
+    num_batches = len(imlist)//batch_size + 1
+    im_batches = []
+    for batch in range(num_batches):
+        batchx = torch.zeros(1, 3, *network_dim)
+        for img in range(batch_size):
+            id = batch*batch_size + img
+            try:
+                image = imlist[id]
+            except IndexError:
+                break
+            inp_image = prep_image(image, network_dim)
+            if img == 0:
+                batchx.copy_(inp_image.data)
+            else:
+                batchx = torch.cat((batchx, inp_image.data))
+        im_batches.append(Variable(batchx))
+    return im_batches
