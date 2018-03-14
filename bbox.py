7771c40768042e594a14b5dab457cd72f1836331
Ayoosh Kathuria <kathuria.ayoosh@gmail.com>
2018-03-14 17:35:59 +0530

    Function to calculate Intersection over union for two boxes

--- bbox.py
+++ bbox.py
@@ -14,4 +14,48 @@ def confidence_filter(result, confidence):
     
     return result
 
+def bbox_iou(box1, box2, corner_coord = True):
+    """
+    Returns the IoU of two bounding boxes describe by a 
+    
+    (x,y,w,h) tuple, if corner_coord is False
+    
+    (x1, y1, x2, y2) representing corner of the box, if corner_coord is True
+    
+    """
+    
+    
+    
+    if not corner_coord:
+        box1 = get_abs_coord(box1)
+        
+        box2 = get_abs_coord(box2)
+    else:
+        #sanity check so that the corner co-rdinates are
+        #aligned towards the diagnal that lies in the 1st quadrant 
+        #If the deal about quadrant puzzles you, ignore
+        #Or maybe time to brush up some co-ordinate geometry.
+        box1 = sanity_fix(box1)
+        box2 = sanity_fix(box2)
+        
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2
+    
+    inter_rect_x1 =  max(b1_x1, b2_x1)
+    inter_rect_y1 =  max(b1_y1, b2_y1)
+    inter_rect_x2 =  min(b1_x2, b2_x2)
+    inter_rect_y2 =  min(b1_y2, b2_y2)
+    
+    inter_area = (inter_rect_x2 - inter_rect_x1 + 1)*(inter_rect_y2 - inter_rect_y1 + 1)
+    
+    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+    print(b1_area)
+    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+    print(b2_area)
+    
+    iou = inter_area / (b1_area + b2_area - inter_area)
+    
+    return iou
+
+
 
