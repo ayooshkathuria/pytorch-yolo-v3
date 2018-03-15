84cd0cfde8793da75b1793cc5d7bfdd87380c251
Ayoosh <kathuria.ayoosh@gmail.com>
2018-03-15 17:01:09 +0530

    Vectorised the bbox_iou function

--- bbox.py
+++ bbox.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F 
 from torch.autograd import Variable
 import numpy as np
+from util import load_classes
 
 
 def confidence_filter(result, confidence):
@@ -33,44 +34,25 @@ def sanity_fix(box):
         
     return box
 
-def bbox_iou(box1, box2, corner_coord = True):
+def bbox_iou(box1, box2):
     """
-    Returns the IoU of two bounding boxes describe by a 
+    Returns the IoU of two bounding boxes 
     
-    (x,y,w,h) tuple, if corner_coord is False
-    
-    (x1, y1, x2, y2) representing corner of the box, if corner_coord is True
     
     """
     
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
     
-    
-    if not corner_coord:
-        box1 = get_abs_coord(box1)
-        
-        box2 = get_abs_coord(box2)
-    else:
-        #sanity check so that the corner co-rdinates are
-        #aligned towards the diagnal that lies in the 1st quadrant 
-        #If the deal about quadrant puzzles you, ignore
-        #Or maybe time to brush up some co-ordinate geometry.
-        box1 = sanity_fix(box1)
-        box2 = sanity_fix(box2)
-        
-    b1_x1, b1_y1, b1_x2, b1_y2 = box1
-    b2_x1, b2_y1, b2_x2, b2_y2 = box2
-    
-    inter_rect_x1 =  max(b1_x1, b2_x1)
-    inter_rect_y1 =  max(b1_y1, b2_y1)
-    inter_rect_x2 =  min(b1_x2, b2_x2)
-    inter_rect_y2 =  min(b1_y2, b2_y2)
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
     
     inter_area = (inter_rect_x2 - inter_rect_x1 + 1)*(inter_rect_y2 - inter_rect_y1 + 1)
     
     b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
-    print(b1_area)
     b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
-    print(b2_area)
     
     iou = inter_area / (b1_area + b2_area - inter_area)
     
@@ -93,5 +75,98 @@ def pred_abs_coord(prediction):
     
     prediction[ind_nz[0], ind_nz[1]] = box
     
-    print(prediction[ind_nz[0], ind_nz[1]][:,:4])
     return prediction
+
+def unique(tensor):
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)
+    unique_tensor = torch.from_numpy(unique_np)
+    
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+    return tensor_res
+
+
+def write_results(prediction, num_classes, nms = True, nms_conf = 0.4):
+    
+    batch_size = prediction.size(0)
+    
+    output = prediction.new(1, prediction.size(2) + 1)
+    write = False
+    
+    for ind in range(batch_size):
+        #select the image from the batch
+        image_pred = prediction[ind]
+        
+        
+        #Get the class having maximum score, and the number of that class
+        #Get rid of num_classes seperate scores 
+        #Add the class index and the class score of class having maximum score
+        max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
+        max_conf = max_conf.float().unsqueeze(1)
+        max_conf_score = max_conf_score.float().unsqueeze(1)
+        seq = (image_pred[:,:5], max_conf, max_conf_score)
+        image_pred = torch.cat(seq, 1)
+        
+        #Get rid of the zero entries
+        non_zero_ind =  (torch.nonzero(image_pred[:,4]))
+        image_pred_ = image_pred[non_zero_ind.squeeze(),:]
+        
+        #Get the various classes detected in tge image
+        img_classes = unique(image_pred_[:,-1])
+        
+        #Get the class names
+        classes = load_classes('data/voc.names')
+                
+            
+        #WE will do NMS classwise
+        for cls in img_classes:
+            #get the detections with one particular class
+            cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
+            class_mask_ind = torch.nonzero(cls_mask[:,-1]).squeeze()
+            image_pred_class = image_pred_[class_mask_ind]
+        
+             #sort the detections such that the entry with the maximum class
+             #confidence is at the top
+            conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)
+            
+            #if nms has to be done
+            if nms:
+                #For each detection
+                for i in range(idx):
+                    #Get the IOUs of all boxes that come after the one we are looking at 
+                    #in the loop
+                    try:
+                        ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
+                    except ValueError:
+                        break
+        
+                    except IndexError:
+                        break
+                    
+                    #Zero out all the detections that have IoU> treshhold
+                    iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                    image_pred_class[i+1:] *= iou_mask       
+                    
+                    #Remove the non-zero entries
+                    non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
+                    image_pred_class = image_pred_class[non_zero_ind]
+                    
+                    
+            
+            
+            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
+            seq = batch_ind, image_pred_class
+            
+            if not write:
+                output = torch.cat(seq,1)
+                write = True
+            else:
+                out = torch.cat(seq,1)
+                output = torch.cat((output,out))
+        
+                
+                    
+    return output
