903e9e4623c0d3691b43e0e333bd62795f55ff04
Ayoosh <kathuria.ayoosh@gmail.com>
2018-03-16 15:56:02 +0530

    Function to include max. class confidence in threshold

--- bbox.py
+++ bbox.py
@@ -9,11 +9,27 @@ from util import load_classes
 
 
 def confidence_filter(result, confidence):
-
     conf_mask = (result[:,:,4] > confidence).float().unsqueeze(2)
     result = result*conf_mask    
     
     return result
+
+def confidence_filter_cls(result, confidence):
+    max_scores = torch.max(result[:,:,5:25], 2)[0]
+    res = torch.cat((result, max_scores),2)
+    print(res.shape)
+    
+    
+    cond_1 = (res[:,:,4] > confidence).float()
+    cond_2 = (res[:,:,25] > 0.995).float()
+    
+    conf = cond_1 + cond_2
+    conf = torch.clamp(conf, 0.0, 1.0)
+    conf = conf.unsqueeze(2)
+    result = result*conf   
+    return result
+
+
 
 def get_abs_coord(box):
     box[2], box[3] = abs(box[2]), abs(box[3])
@@ -40,17 +56,20 @@ def bbox_iou(box1, box2):
     
     
     """
-    
+    #Get the coordinates of bounding boxes
     b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
     b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
     
+    #get the corrdinates of the intersection rectangle
     inter_rect_x1 =  torch.max(b1_x1, b2_x1)
     inter_rect_y1 =  torch.max(b1_y1, b2_y1)
     inter_rect_x2 =  torch.min(b1_x2, b2_x2)
     inter_rect_y2 =  torch.min(b1_y2, b2_y2)
     
+    #Intersection area
     inter_area = (inter_rect_x2 - inter_rect_x1 + 1)*(inter_rect_y2 - inter_rect_y1 + 1)
     
+    #Union Area
     b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
     b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
     
@@ -97,10 +116,10 @@ def write_results(prediction, num_classes, nms = True, nms_conf = 0.4):
     for ind in range(batch_size):
         #select the image from the batch
         image_pred = prediction[ind]
+
         
-        
-        #Get the class having maximum score, and the number of that class
-        #Get rid of num_classes seperate scores 
+        #Get the class having maximum score, and the index of that class
+        #Get rid of num_classes softmax scores 
         #Add the class index and the class score of class having maximum score
         max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
         max_conf = max_conf.float().unsqueeze(1)
@@ -108,25 +127,27 @@ def write_results(prediction, num_classes, nms = True, nms_conf = 0.4):
         seq = (image_pred[:,:5], max_conf, max_conf_score)
         image_pred = torch.cat(seq, 1)
         
+        
         #Get rid of the zero entries
         non_zero_ind =  (torch.nonzero(image_pred[:,4]))
         image_pred_ = image_pred[non_zero_ind.squeeze(),:]
         
-        #Get the various classes detected in tge image
+        #Get the various classes detected in the image
         img_classes = unique(image_pred_[:,-1])
         
         #Get the class names
         classes = load_classes('data/voc.names')
                 
-            
         #WE will do NMS classwise
         for cls in img_classes:
             #get the detections with one particular class
             cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
             class_mask_ind = torch.nonzero(cls_mask[:,-1]).squeeze()
+
             image_pred_class = image_pred_[class_mask_ind]
+
         
-             #sort the detections such that the entry with the maximum class
+             #sort the detections such that the entry with the maximum objectness
              #confidence is at the top
             conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
             image_pred_class = image_pred_class[conf_sort_index]
@@ -146,7 +167,7 @@ def write_results(prediction, num_classes, nms = True, nms_conf = 0.4):
                     except IndexError:
                         break
                     
-                    #Zero out all the detections that have IoU> treshhold
+                    #Zero out all the detections that have IoU > treshhold
                     iou_mask = (ious < nms_conf).float().unsqueeze(1)
                     image_pred_class[i+1:] *= iou_mask       
                     
@@ -156,7 +177,11 @@ def write_results(prediction, num_classes, nms = True, nms_conf = 0.4):
                     
                     
             
-            
+            #Concatenate the batch_id of the image to the detection
+            #this helps us identify which image does the detection correspond to 
+            #We use a linear straucture to hold ALL the detections from the batch
+            #the batch_dim is flattened
+            #batch is identified by extra batch column
             batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
             seq = batch_ind, image_pred_class
             
@@ -166,7 +191,5 @@ def write_results(prediction, num_classes, nms = True, nms_conf = 0.4):
             else:
                 out = torch.cat(seq,1)
                 output = torch.cat((output,out))
-        
-                
-                    
+    
     return output
