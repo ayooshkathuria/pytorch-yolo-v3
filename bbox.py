809bce0fb225e1291e1dfea32e667c97e74be899
Ayoosh Kathuria <kathuria.ayoosh@gmail.com>
2018-03-18 03:14:17 +0530

    Moved the write_results to utils.py

--- bbox.py
+++ bbox.py
@@ -1,11 +1,8 @@
 from __future__ import division
 
 import torch 
-import torch.nn as nn
-import torch.nn.functional as F 
-from torch.autograd import Variable
+
 import numpy as np
-from util import load_classes
 
 
 def confidence_filter(result, confidence):
@@ -96,100 +93,6 @@ def pred_corner_coord(prediction):
     
     return prediction
 
-def unique(tensor):
-    tensor_np = tensor.cpu().numpy()
-    unique_np = np.unique(tensor_np)
-    unique_tensor = torch.from_numpy(unique_np)
-    
-    tensor_res = tensor.new(unique_tensor.shape)
-    tensor_res.copy_(unique_tensor)
-    return tensor_res
 
 
-def write_results(prediction, num_classes, nms = True, nms_conf = 0.4):
-    
-    batch_size = prediction.size(0)
-    
-    output = prediction.new(1, prediction.size(2) + 1)
-    write = False
-    
-    for ind in range(batch_size):
-        #select the image from the batch
-        image_pred = prediction[ind]
 
-        
-        #Get the class having maximum score, and the index of that class
-        #Get rid of num_classes softmax scores 
-        #Add the class index and the class score of class having maximum score
-        max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
-        max_conf = max_conf.float().unsqueeze(1)
-        max_conf_score = max_conf_score.float().unsqueeze(1)
-        seq = (image_pred[:,:5], max_conf, max_conf_score)
-        image_pred = torch.cat(seq, 1)
-        
-        
-        #Get rid of the zero entries
-        non_zero_ind =  (torch.nonzero(image_pred[:,4]))
-        image_pred_ = image_pred[non_zero_ind.squeeze(),:]
-        
-        #Get the various classes detected in the image
-        img_classes = unique(image_pred_[:,-1])
-        
-        #Get the class names
-        classes = load_classes('data/voc.names')
-                
-        #WE will do NMS classwise
-        for cls in img_classes:
-            #get the detections with one particular class
-            cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
-            class_mask_ind = torch.nonzero(cls_mask[:,-1]).squeeze()
-
-            image_pred_class = image_pred_[class_mask_ind]
-
-        
-             #sort the detections such that the entry with the maximum objectness
-             #confidence is at the top
-            conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
-            image_pred_class = image_pred_class[conf_sort_index]
-            idx = image_pred_class.size(0)
-            
-            #if nms has to be done
-            if nms:
-                #For each detection
-                for i in range(idx):
-                    #Get the IOUs of all boxes that come after the one we are looking at 
-                    #in the loop
-                    try:
-                        ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
-                    except ValueError:
-                        break
-        
-                    except IndexError:
-                        break
-                    
-                    #Zero out all the detections that have IoU > treshhold
-                    iou_mask = (ious < nms_conf).float().unsqueeze(1)
-                    image_pred_class[i+1:] *= iou_mask       
-                    
-                    #Remove the non-zero entries
-                    non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
-                    image_pred_class = image_pred_class[non_zero_ind]
-                    
-                    
-            
-            #Concatenate the batch_id of the image to the detection
-            #this helps us identify which image does the detection correspond to 
-            #We use a linear straucture to hold ALL the detections from the batch
-            #the batch_dim is flattened
-            #batch is identified by extra batch column
-            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
-            seq = batch_ind, image_pred_class
-            
-            if not write:
-                output = torch.cat(seq,1)
-                write = True
-            else:
-                out = torch.cat(seq,1)
-                output = torch.cat((output,out))
-    
-    return output
