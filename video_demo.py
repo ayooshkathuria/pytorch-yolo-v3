abeeb8766d70f400650ca22255f3a1b0976175f5
Ayoosh <kathuria.ayoosh@gmail.com>
2018-03-20 12:55:12 +0530

    Module to perform detection on a video

---
+++ video_demo.py
@@ -0,0 +1,16 @@
+from utils import *
+from darknet import Darknet
+import cv2
+
+def get_test_input(input_dim, CUDA):
+    img = cv2.imread("dog-cycle-car.png")
+    img = cv2.resize(img, (input_dim, input_dim)) 
+    img_ =  img[:,:,::-1].transpose((2,0,1))
+    img_ = img_[np.newaxis,:,:,:]/255.0
+    img_ = torch.from_numpy(img_).float()
+    img_ = Variable(img_)
+    
+    if CUDA:
+        img_ = img_.cuda()
+    
+    return img_
