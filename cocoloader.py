ee0db1715d513a98f49e522fcdc2207e06b9db19
Ayoosh <kathuria.ayoosh@gmail.com>
2018-07-17 16:48:55 +0530

    Cocoloader to load files from COCO

--- cocoloader.py
+++ cocoloader.py
@@ -1,7 +1,11 @@
 import torch 
 from torchvision.datasets import CocoDetection
 import os
-
+import numpy as np
+import pickle as pkl
+import matplotlib.pyplot as plt
+from data_aug.bbox_util import draw_rect
+from data_aug.data_aug import *
 
 class CocoDataset(CocoDetection):
     def __init__(self, root, annFile, det_transforms = None):
@@ -20,7 +24,53 @@ class CocoDataset(CocoDetection):
 #print(filename)
 #print(os.path.exists(filename))
 
-cocoloader = CocoDataset(root = "../cocoapi/train2017", annFile = "../cocoapi/annotations/instances_train2017.json")
+#cocoloader = CocoDataset(root = "../cocoapi/train2017", annFile = "../cocoapi/annotations/instances_train2017.json")
+
+def tiny_coco(cocoloader, num):
+    i = 0
+    li = []
+    
+    for x in cocoloader:
+        print(i)
+        li.append(x)
+        i += 1
+        if i > num - 1:
+            break
+    num = num / 1000
+    pkl.dump(li, open("COCO_{}k.pkl.format(num)", "wb"))
 
 
-        
+coco_loader = pkl.load(open("COCO_100.pkl", "rb"))
+
+def trasform_annotation(x):
+    #convert the PIL image to a numpy array
+    image = np.array(x[0])
+    
+    #get the bounding boxes and convert them into proper format 
+    boxes = [a["bbox"] for a in x[1]]
+    
+    boxes = np.array(boxes)
+    
+    boxes = boxes.reshape(-1,4)
+    
+    boxes[:,2] += boxes[:,0]
+    boxes[:,3] += boxes[:,1]
+
+    
+    category_ids = [a["category_id"] for a in x[1]]
+    
+    return image, boxes, category_ids
+    
+    
+transforms = Sequence([RandomHorizontalFlip(), RandomScaleTranslate(translate=0.05, scale=(0,0.3)), RandomRotate(10), RandomShear(), YoloResize(448)])
+    
+i = 0
+for x in coco_loader:
+    x = trasform_annotation(x)
+    a = transforms(x[0], x[1])
+    im = draw_rect(a[0], a[1])
+    plt.imshow(im)	
+    plt.show()
+    i += 1
+    if i == 10:
+        break
